688d0a9b124d7a028f161e6805a4747413621157
Henrique Gogó <henriquegogo@gmail.com>
2016-09-11 22:22:38 -0300

    Adding query string parameters

--- main.py
+++ main.py
@@ -2,15 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import requests, re, json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/favicon.ico")
+def favicon():
+    return ''
+
 @app.route("/<hashtag>")
 def hashtag(hashtag):
+    text = request.args.get('text') or ''
+    duration = request.args.get('duration') or 5000
     url = 'https://www.instagram.com/explore/tags/{}/'.format(hashtag)
     response_body = requests.get(url).text
     match = re.search('>window._sharedData = (.*);<', response_body)
@@ -20,8 +26,7 @@ def hashtag(hashtag):
     photos_list = map(lambda item: { 'src': item['display_src'], 'text': item['caption'] }, timeline_nodes)
     photos_list_json = json.dumps(photos_list)
 
-    print(photos_list_json)
-    return render_template('index.html', list=photos_list_json)
+    return render_template('index.html', list=photos_list_json, text=text, duration=duration)
 
 if __name__ == "__main__":
     app.run()
