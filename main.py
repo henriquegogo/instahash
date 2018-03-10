e29b3ca2bb8c93bc030806ec9da3f3ed6110f00e
Henrique Gogó <henriquegogo@gmail.com>
2018-03-10 13:38:21 -0300

    Compatible with new instagram version

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def hashtag(hashtag):
     match = re.search('>window._sharedData = (.*);<', response_body)
     json_string = match.group(1)
     timeline_object = json.loads(json_string)
-    timeline_nodes = timeline_object['entry_data']['FeedPage'][0]['graphql']['user']['edge_web_feed_timeline']['edges']
-    timeline_nodes = filter(lambda item: item['is_video'] == False and int(item['date']) > startdate, timeline_nodes)
-    photos_list = map(lambda item: { 'src': item['display_src'], 'text': item['caption'] }, timeline_nodes)
+    timeline_nodes = timeline_object['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+    timeline_nodes = filter(lambda item: item['node']['is_video'] == False and int(item['node']['taken_at_timestamp']) > startdate, timeline_nodes)
+    photos_list = map(lambda item: { 'src': item['node']['display_url'], 'text': item['node']['edge_media_to_caption']['edges'][0]['node']['text'] }, timeline_nodes)
     photos_list_json = json.dumps(photos_list)
 
     return render_template('show.html', list=photos_list_json, text=text, duration=duration, customization=customization)
